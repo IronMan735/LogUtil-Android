3648303cfa687cab5ee7eb62ade8ef12d948fb3f
Aria <airani926@gmail.com>
2020-04-14 16:56:56 +0430

    Make The Function Static

--- logutil/src/main/java/com/example/logutil/LogDebug.kt
+++ logutil/src/main/java/com/example/logutil/LogDebug.kt
@@ -2,8 +2,10 @@ package com.example.logutil
 
 import android.util.Log
 
-open class LogDebug {
-    open fun showLog(tag:String, msg:String) {
-        Log.d(tag, msg)
+class LogDebug {
+    companion object {
+        fun showLog(tag:String, msg:String) {
+            Log.d(tag, msg)
+        }
     }
 }
